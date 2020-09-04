40c98a4c270321b55fb9fbc8b8a54bc4790df97b
Razzak <zackmarko2@gmail.com>
2020-09-04 20:50:35 +0530

    First commit

---
+++ lib/services/auth.dart
@@ -0,0 +1,40 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user object based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+  }
+
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
+
+//Sign in anon
+
+//Register with email & pass
+
+// Sign in with email & pass
